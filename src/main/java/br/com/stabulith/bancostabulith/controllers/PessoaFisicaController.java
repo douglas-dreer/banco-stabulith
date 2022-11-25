ead0f38ddba8b83fd2e688a907acafc9fe5045d2
douglas.dreer <douglas.dreer@fornecedores.sicoob.com.br>
2022-11-25 14:49:44 -0300

    Adicionado a funcionalidade de editar e excluir à PessoaFisica

--- src/main/java/br/com/stabulith/bancostabulith/controllers/PessoaFisicaController.java
+++ src/main/java/br/com/stabulith/bancostabulith/controllers/PessoaFisicaController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -40,12 +41,13 @@ public class PessoaFisicaController {
 
     @PutMapping("/{id}")
     public ResponseEntity<PessoaFisicaDTO> editar(@PathVariable UUID id, @RequestBody PessoaFisicaDTO dto) throws URISyntaxException {
+        dto.setDataModificacao(LocalDateTime.now());
         dto = pessoaFisicaService.salvar(dto);
         return ResponseEntity.created(new URI(String.format("%s/%s", httpServletRequest.getRequestURL(), dto.getId()))).build();
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<PessoaFisicaDTO> excluir(@PathVariable UUID id) throws URISyntaxException {
+    public ResponseEntity<PessoaFisicaDTO> excluir(@PathVariable UUID id) {
         pessoaFisicaService.excluir(id);
         return ResponseEntity.noContent().build();
     }
