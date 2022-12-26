ebcd2194fd86d9b53c4f09322aa4ece0f006272b
Douglas <douglasdreer@outlook.com.br>
2022-12-26 13:46:31 -0300

    Ajustado o retorno do método de delete para voltar 202 (Accept)

--- src/main/java/br/com/stabulith/bancostabulith/controllers/PessoaJuridicaController.java
+++ src/main/java/br/com/stabulith/bancostabulith/controllers/PessoaJuridicaController.java
@@ -82,7 +82,7 @@ public class PessoaJuridicaController {
             log.info(NAO_FOI_ENCONTRADO_REGISTROS.getDescricao());
             return ResponseEntity.notFound().build();
         }
-        return ResponseEntity.ok(null);
+        return ResponseEntity.accepted().build();
     }
 
 
